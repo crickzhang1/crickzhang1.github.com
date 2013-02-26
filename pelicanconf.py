d00b9cc06a7e98bac08d6e7aa31745800b35ced2
Yinhe Zhang <crickzhang1@gmail.com>
2013-02-26 20:46:38 +0800

    Change some Pelican settings to fit my need.
    1. Articles date now shows time.
    2. Posts are generated in Year/Month/Day directories.
    3. Change SITEURL to blog.

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,11 +3,11 @@
 
 AUTHOR = u'Crick Zhang'
 SITENAME = u"Crickzhang's Blog"
-SITEURL = 'http://crickzhang1.github.com'
+SITEURL = 'http://crickzhang1.github.com/blog'
 
 TIMEZONE = 'Asia/Shanghai'
 
-DEFAULT_LANG = u'en'
+DEFAULT_LANG = u'zh_CN.utf8'
 
 # Blogroll
 LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
@@ -25,3 +25,8 @@ USE_FOLDER_AS_CATEGORY = True
 SUMMARY_MAX_LENGTH = 100
 
 DISPLAY_PAGES_ON_MENU = True
+
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+
+DEFAULT_DATE_FORMAT = '%H:%M, %a %d %B %Y'
